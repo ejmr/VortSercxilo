9b3c7be7e614bdd11fe51e8e9916f9a0e5a7cd27
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-07-21 22:48:03 -0400

    Download the dictionary file
    This patch contains the first version of the program.  It cannot
    look-up dictionary entries.  All it can do is download the dictionary
    file when given the ‘--download-dictionary’ argument.

---
+++ vortsercxilo.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import argparse
+import urllib.request
+
+__version__ = "0.1.0"
+
+# These two global variables provide the URI to the 'ESPDIC.txt' file,
+# i.e. the Esperanto-English dictionary, and the filename we want to
+# use for our local copy of the dictionary.
+DICTIONARY_URI = "http://www.denisowski.org/Esperanto/ESPDIC/espdic.txt"
+DICTIONARY_FILENAME = "ESPDIC.txt"
+
+def download_dictionary():
+    """Download a local copy of the Esperanto-English dictionary."""
+    with urllib.request.urlopen(DICTIONARY_URI) as remote:
+        with open(DICTIONARY_FILENAME, mode="w", encoding="utf-8") as local:
+            local.write(remote.read().decode("utf-8"))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--download-dictionary", help="download the latest dictionary", action="store_true")
+    parser.add_argument("--version", action="version", version="%(prog)s {0}".format(__version__))
+    arguments = parser.parse_args()
+
+    if arguments.download_dictionary is True:
+        download_dictionary()
