f14c52d1403ee6a5b99d79e24211fcfb6633a665
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-07-24 23:15:52 -0400

    Prepare version 1.0.0 by fixing word matching
    This patch correctly the regular expressions built via the ‘--match’
    argument as they had some issues, such as a misuse of ‘$’ and a
    failure to match letters at the start of words by not allowing
    arbitrary characters to follow them, and so on.
    
    The patch also removes ‘--download-dictionary’.  Initially the option
    was useful to me.  But when preparing to release the program I
    realized it was only useful for testing.  The more I thought about it
    the more I came to the opinion that users would never need it.

--- vortsercxilo.py
+++ vortsercxilo.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
+"""This script is the implementation of VortSerĉilo.  If there are no
+errors the program exits with the system code zero.  But if something
+goes wrong, e.g. download the dictionary, then Python itself will halt
+due to uncaught exceptions.
+
+"""
+
 import argparse
 import os.path
 import re
 import urllib.request
 
-__version__ = "0.5.0"
+__version__ = "1.0.0"
 
 # These two global variables provide the URI to the 'ESPDIC.txt' file,
 # i.e. the Esperanto-English dictionary, and the filename we want to
@@ -13,11 +20,15 @@ __version__ = "0.5.0"
 DICTIONARY_URI = "http://www.denisowski.org/Esperanto/ESPDIC/espdic.txt"
 DICTIONARY_FILENAME = "ESPDIC.txt"
 
+
+
 def download_dictionary():
     """Download a local copy of the Esperanto-English dictionary."""
     with urllib.request.urlopen(DICTIONARY_URI) as remote:
         with open(DICTIONARY_FILENAME, mode="w", encoding="utf-8") as local:
             local.write(remote.read().decode("utf-8"))
+
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -25,28 +36,33 @@ if __name__ == '__main__':
     parser.add_argument("--match", default="start",
                         choices=["start", "end", "anywhere"],
                         help="searchs for the word at the beginning, end, or anywhere in words")
-    parser.add_argument("--download-dictionary", help="download the latest dictionary", action="store_true")
     parser.add_argument("--version", action="version", version="%(prog)s {0}".format(__version__))
     arguments = parser.parse_args()
-
-    if arguments.download_dictionary is True:
-        download_dictionary()
 
     if os.path.exists(DICTIONARY_FILENAME) is False:
         download_dictionary()
 
     with open(DICTIONARY_FILENAME, mode="r", encoding="utf-8") as espdic:
 
+        # ESPDIC uses the format
+        #
+        #     Esperanto : English, English, English...
+        #
+        # for all entries.  We add a test for that colon to all
+        # regular expressions so that they only match the Esperanto.
+        # We also assign a search function so that we do not have to
+        # write a similar but redundant if-elif block when reading
+        # from ESPDIC.
         if arguments.match == "start":
-            word = re.compile(arguments.word + r"\s+:", re.IGNORECASE)
+            word = re.compile(arguments.word + r"\B.+\s+:", re.IGNORECASE)
             search_function = re.match
         elif arguments.match == "end":
-            word = re.compile(arguments.word + r"$\s+:", re.IGNORECASE)
+            word = re.compile(".+" + arguments.word + r"\b\s+:", re.IGNORECASE)
             search_function = re.search
         elif arguments.match == "anywhere":
             word = re.compile(arguments.word + r"\s+:", re.IGNORECASE)
             search_function = re.search
-        
+
         for entry in espdic.readlines():
             match = search_function(word, entry)
             if match: print(match.string, end="")
